fa3a5f1b237f76de9f86def95968cd8a94f36f30
htoth <31545229+htoth18@users.noreply.github.com>
2017-09-15 12:01:12 -0400

    Fixed Training Controller merge conflicts

--- HumanArcCompliance/HumanArcCompliance/Controllers/TrainingController.cs
+++ HumanArcCompliance/HumanArcCompliance/Controllers/TrainingController.cs
@@ -6,24 +6,20 @@ using System.Web.Mvc;
 
 namespace HumanArcCompliance.Controllers
 {
-    public class TrainingController: Controller
+    public class TrainingController : Controller
     {
         public ActionResult Training()
         {
             return View();
         }
 
-<<<<<<< HEAD
-		public ActionResult EditTraining()
-		{
-			return View();
-		}
-=======
+        public ActionResult EditTraining()
+        {
+            return View();
+        }
         public ActionResult AddTraining()
         {
             return View();
         }
->>>>>>> 3965c7db1b50b3d44a9448c94bfc4448f2b8436c
-    
     }
 }
