8776fce2c5fec34e34057de61e89d6660636c7bd
Jae Son <sjson421@gmail.com>
2017-09-27 11:52:11 -0400

    Added nonfunctional quiz page.
    Buggy.

---
+++ HumanArcCompliance/HumanArcCompliance/Scripts/Quiz.js
@@ -0,0 +1,67 @@
+﻿var Myapp = angular.module('Quizapp', []);
+Myapp.controller('QuizCtrl', ['$scope', '$http', 'getQuestion', function (scope, http, getQuestion) {
+    scope.QuestionAnswer = []
+    scope.count = 0;
+    scope.correctAns = 0;
+    scope.checkedCount = 0;
+    scope.notvisible = true;
+    scope.questionsvisible = true;
+    //get the Question and answers from the controller and pass it to array  
+    scope.rbshow = false;
+    getQuestion.result().success(function (response) {
+        for (var i = 0; i < response.length; i++) {
+            scope.QuestionAnswer.push(response[i]);
+        }
+    });
+    scope.nextQuestion = function () {
+        scope.rbshow = true;
+        var nextQuestion = scope.count; //To get next question  
+        var answer = "";
+        var choicesArr = document.getElementsByName('choices');
+        for (var i = 0; i < choicesArr.length; i++) {
+            if (choicesArr[i].checked) {
+                scope.checkedCount = scope.checkedCount + 1;
+                answer = scope.QuestionAnswer[nextQuestion - 1].Ans;
+                if (choicesArr[i].value == answer) {
+                    scope.correctAns = scope.correctAns + 1;
+                }
+                choicesArr[i].checked = false;
+            }
+        }
+        if (scope.checkedCount == scope.QuestionAnswer.length) {
+            scope.notvisible = false;
+            scope.questionsvisible = false;
+        }
+        else if (scope.QuestionAnswer.length >= scope.count) {
+            scope.QuestionAnswer.Question = scope.QuestionAnswer[nextQuestion].Question;
+            scope.QuestionAnswer.OptionA = scope.QuestionAnswer[nextQuestion].OpA;
+            scope.QuestionAnswer.OptionB = scope.QuestionAnswer[nextQuestion].OpB;
+            scope.QuestionAnswer.OptionC = scope.QuestionAnswer[nextQuestion].OpC;
+            scope.count = scope.count + 1;
+        }
+    }
+}]);
+//http get to retrive the questions from Home Controller QuizQuestionAns Action //Methood  
+Myapp.service('getQuestion', function ($http) {
+    this.result = function () {
+        var ans = $http.get('Training/QuizQuestionAns');
+        return ans;
+    }
+});
+//Directive with radio button to select the option  
+//Submit button to go to next question  
+//This directive displays the question along with the options to select  
+Myapp.directive("questionContainer", function () {
+    return {
+        template: '<div>{{QuestionAnswer.Question}}<br/>' + '<input name="choices" name="choices" type="radio" value="A" ng-show="rbshow" />{{QuestionAnswer.OptionA}}<br/>' + '<input name="choices" name="choices" type="radio" value="B" ng-show="rbshow" />{{QuestionAnswer.OptionB}}<br/>' + '<input name="choices" name="choices" type="radio" value="C" ng-show="rbshow" />{{QuestionAnswer.OptionC}}</br>' + ' <input type="button" ng-click="nextQuestion()" value="Next"/>' + '</div>',
+        restrict: "E"
+    }
+});
+//Directive is shown at last once the quiz is completed with the total number of correct answers  
+Myapp.directive("correctContainer", function () {
+    return
+    {
+        template: '<div>Total Correct Answer {{correctAns}}<br/>',
+            restrict: "E"
+    }
+}  
--- HumanArcCompliance/HumanArcCompliance/Scripts/TrainingDataset.js
+++ HumanArcCompliance/HumanArcCompliance/Scripts/TrainingDataset.js
@@ -1,10 +1,8 @@
 ﻿//Dataset for To Do list
-var downloadFile = "<a href='download.html'>Download Training File</a>";
-var takeQuiz = "<a href='quiz.html'>Take Quiz</a";
 
 var toDoDataSet = [
-    ["Learn Assembly", downloadFile, takeQuiz, "24%"],
-    ["Learn Whitespace....why", downloadFile, takeQuiz, "53%"]
+    ["Learn Assembly", "<a href='download.html'>Download Training File</a>", "<a href='Quiz'>Take Quiz</a>", "24%"],
+    ["Learn Whitespace....why", "<a href='download.html'>Download Training File</a>", "<a href='Quiz'>Take Quiz</a>", "53%"]
 ];
 
 $(document).ready(function () {
@@ -20,12 +18,10 @@ $(document).ready(function () {
 });
 
 //Dataset for Completed list
-var downloadFileCompleted = "<a href='download.html'>Download Training File</a>";
-
 var completedDataSet = [
-    ["How to wash your hands", downloadFileCompleted],
-    ["How to drink water", downloadFileCompleted],
-    ["How to breathe air", downloadFileCompleted]
+    ["How to wash your hands", "<a href='download.html'>Download Training File</a>"],
+    ["How to drink water", "<a href='download.html'>Download Training File</a>"],
+    ["How to breathe air", "<a href='download.html'>Download Training File</a>"]
 ];
 
 $(document).ready(function () {
