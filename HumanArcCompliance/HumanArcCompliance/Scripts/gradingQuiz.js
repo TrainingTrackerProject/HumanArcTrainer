295fd2bb8cf7f1f51acb8245bde96ca174c2420a
haylietoth <31545229+haylietoth@users.noreply.github.com>
2017-11-10 11:54:35 -0500

    Grading updates and query
    Query is in queries folder

--- HumanArcCompliance/HumanArcCompliance/Scripts/gradingQuiz.js
+++ HumanArcCompliance/HumanArcCompliance/Scripts/gradingQuiz.js
@@ -1,37 +1,31 @@
 ﻿var userData = [];
 $(document).ready(function () {
-    getAllUsers();
-
-    $('#gradeEmpTable tbody').on('click', 'tr', function () {
-
-        var data = $('#gradeEmpTable').DataTable().row(this).data()
-
+    $('#employeGradeTable tbody').on('click', 'tr', function () { 
+        var data = $('#employeGradeTable').DataTable().row(this).data()
         alert('You clicked on ' + data[0] + '\'s row');
-        //display user quiz table
-            //if user quiz is click, display results 
-
     });
+    getAllUsers();
 });
 
 getAllUsers = function () {
-    $.ajax({
-        url: '/Training/GetAllUsers',
-        type: 'GET',
-        cache: false,
-        contentType: 'application/json; charset=utf-8',
-        success: function (data, status) {
-            fillDataTable(data);
-        },
-        error: function () {
-        }
-    });
+        $.ajax({
+            url: '/Training/GetAllUsers',
+            type: 'GET',
+            cache: false,
+            contentType: 'application/json; charset=utf-8',
+            success: function (data, status) {
+                fillDataTable(data);
+            },
+            error: function () {
+            }
+        });
 }
 
 fillDataTable = function (users) {
     $.each(users, function (index, value) {
-        userData.push([value.givenName + " " + value.sn, value.sAMAccountName, value.manager]);
+        userData.push([value.firstName + " " + value.lastName, value.SAMAccountName, value.manager]);
     });
-    $('#gradeEmpTable').DataTable({
+    $('#employeGradeTable').DataTable({
         data: userData,
         columns: [
             { title: "Name" },
@@ -40,4 +34,3 @@ fillDataTable = function (users) {
         ]
     });
 }
-
