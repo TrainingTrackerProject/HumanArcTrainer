8eafad74bbf1a9be5e0262c3bea59b698436cad3
Jae Son <sjson421@gmail.com>
2017-11-19 21:03:26 -0500

    Quiz title and description

--- HumanArcCompliance/HumanArcCompliance/Scripts/gradingQuiz.js
+++ HumanArcCompliance/HumanArcCompliance/Scripts/gradingQuiz.js
@@ -1,4 +1,55 @@
-﻿var userData = [];
+﻿/*
+ * AngularJS code for the Quiz application, called on gradeQuiz.cshtml.
+ */
+
+app = angular.module("QuizApp", []);
+var json = {
+    title: 'my title',
+    description: 'myDesc',
+    allQuestions: [{
+        text: 'HERE IS qUeSTION 1',
+        type: 'multipleChoice',
+        answers: [{
+            text: 'a1',
+            isCorrect: true
+        },
+        {
+            text: 'a2',
+            isCorrect: false
+        },
+        {
+            text: 'a3',
+            isCorrect: false
+        },
+        {
+            text: 'a4',
+            isCorrect: false
+        }]
+    },
+    {
+        text: 'HERE IS qUeSTION 2',
+        type: 'trueFalse',
+        answers: [{
+            text: 'True',
+            isCorrect: true
+        },
+        {
+            text: 'False',
+            isCorrect: false
+        }]
+    },
+    {
+        text: 'HERE IS qUeSTION 3',
+        type: 'shortAnswer'
+    }
+    ]
+}
+app.controller("QuizCtrl", function ($scope) {
+    $scope.isArray = angular.isArray;
+    $scope.json = json;
+})
+
+/*var userData = [];
 $(document).ready(function () {
     getAllUsers();
 
@@ -44,4 +95,4 @@ fillDataTable = function (users) {
             { title: "manager" }
         ]
     });
-}
+}*/
