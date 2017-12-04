6498b7ddc7d0c0d7b8249d045878700943567a4e
ctherrien18 <ctherrien18@jcu.edu>
2017-12-04 15:41:18 -0500

    added View if start date is after today...

--- HumanArcCompliance/HumanArcCompliance/Scripts/AllTraining.js
+++ HumanArcCompliance/HumanArcCompliance/Scripts/AllTraining.js
@@ -11,10 +11,33 @@ $(document).ready(function () {
                 
                 var start = value.startDate;
                 var res = start.slice(6, 19);
+                var startNum = Number(res);
                 var newDate = new Date(parseInt(res, 10));
                 var date = newDate.toDateString();
 
-                quizes.push([value.title, value.description, date, "<input type='button' value='Edit' class='btn btn-primary edit' id='" + value.id + "'/>" + " || " + "<input type='button' value='Remove' class='btn btn-primary remove' id='" + value.id + "'/>"])
+                //If start date > today's date, change Edit value to View
+                var today = new Date();
+                var dd = today.getDate();
+                var mm = today.getMonth() + 1; //January is 0
+                var yyyy = today.getFullYear();
+                if (dd < 10) {
+                    dd = '0' + dd
+                }
+                if (mm < 10) {
+                    mm = '0' + mm
+                }
+                today = mm + '/' + dd + '/' + yyyy;
+
+                var t = Date.parse(today);
+                
+                var button = "<input type='button' value='Edit' class='btn btn-primary edit' id='" + value.id + "'/>";
+
+                if (startNum > t) {
+                    button = "<input type='button' value='View' class='btn btn-primary edit' id='" + value.id + "'/>";
+      
+                };
+
+                quizes.push([value.title, value.description, date, button + " || " + "<input type='button' value='Remove' class='btn btn-primary remove' id='" + value.id + "'/>"]);
             });
             $('#trainingTable').DataTable({
                 data: quizes,
@@ -33,7 +56,7 @@ $(document).ready(function () {
     $("body").on("click", ".edit", function () {
         location.href = 'updateTraining'
     });
-
+    
     var id;
     var row;
     $("body").on("click", ".remove", function () {
