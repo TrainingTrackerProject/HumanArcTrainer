939759a67de52e7252125a86719ff9ea6efa89ce
ctherrien18 <ctherrien18@jcu.edu>
2017-12-10 22:22:59 -0500

    Big Index Page changes

--- HumanArcCompliance/HumanArcCompliance/Scripts/index.js
+++ HumanArcCompliance/HumanArcCompliance/Scripts/index.js
@@ -1,3 +1,25 @@
 ﻿$(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+//Dataset for To Do list for My Training
+
+//Counts number of quizzes user has to still complete
+$.ajax({
+    url: '/Training/GetUserQuizes',
+    type: 'GET',
+    contentType: "application/json",
+    success: function (data, status) {
+        var counter = 0;
+        $.each(data, function (index, value) {
+            if (!value.isGraded && !value.isCompleted) {
+                counter++;  
+            }
+        });  
+
+        $('#quizNum').html(counter);
+
+
+    }
+});
+
