64466bc4263b5a316180a1bf0490e0baeac3f62c
Jae Son <sjson421@gmail.com>
2017-12-12 15:42:27 -0500

    Filled SubmitGrade in GradeQuiz.js

--- HumanArcCompliance/HumanArcCompliance/Scripts/gradeQuiz.js
+++ HumanArcCompliance/HumanArcCompliance/Scripts/gradeQuiz.js
@@ -3,7 +3,7 @@
  */
 var app = angular.module('QuizApp', ['ngRoute']);
 app.controller('QuizCtrl', function ($scope, $http) {
-    var submittedAnswers = [];
+    var gradeApprovals = [];
     var questionIterator = 0;
     $scope.data;
     $scope.quiz = {
@@ -44,10 +44,6 @@ app.controller('QuizCtrl', function ($scope, $http) {
                     }
                 }
             }
-            /*var answer = {
-                answerId: value.answerId,
-                answerText: value.text
-            }*/
         }
     });
 
@@ -58,75 +54,17 @@ app.controller('QuizCtrl', function ($scope, $http) {
         $scope.quiz.questions[i].isApproved = false;
     }
     $scope.submit = function () {
-        $('#confirm-submit').modal('hide');
-        $http.post('/Training/SubmitGrade', { answers: JSON.stringify(/*TODO*/) }, config).then(function (res) {
-        });
-    }
-    /*
-    $scope.submit = function () {
-        $('#confirm-submit').modal('hide');
-        $scope.status.isSubmitted = true;
-        $http.post('/Training/SubmitGrading', { answers: JSON.stringify(submittedAnswers) }, config).then(function (res) {
-            $scope.status.text = res.data;
-        });
-    }
-
-    function displayShortAnswerQuestions() {
-        $scope.quiz.questions.forEach(function (question) {
-            question.questionId = data.question.id;
-            question.questionText = data.question.text;
-            question.type = data.question.type;
-            question.answerText.push(data.question.answerText);
-        })
-    }
-    //function setpossibleanswers() {
-    //    $.each(data.questions[$scope.quiz.currentquestion].answers, function (index, value) {
-    //        var answer = {
-    //            id: value.id,
-    //            text: value.answertext,
-    //        }
-    //        $scope.quiz.question.answers.push(answer);
-    //    });
-    //}
-
-    function setQuestionAnswer() {
-        if ($scope.quiz.question.selectedAnswer != 0) {
-            if (typeof submittedAnswers[$scope.quiz.currentQuestion] === 'undefined') {
-                var uqqa = {
-                    quizId: quizId,
-                    questionId: angular.copy($scope.quiz.question.questionId),
-                    answerId: angular.copy($scope.quiz.question.selectedAnswer),
-                    answerText: ''
-                }
-                submittedAnswers.push(uqqa);
-            } else {
-                submittedAnswers[$scope.quiz.currentQuestion].answerId = angular.copy($scope.quiz.question.selectedAnswer);
+        for (var i = 0; i < $scope.data.questions.length; i++) {
+            var uqqa = {
+                quizId: quizId,
+                userId: userId,
+                questionId: angular.copy($scope.quiz.question.questionId),
+                isApproved: angular.copy($scope.data.questions[i].isApproved)
             }
+            gradeApprovals.push(uqqa);
         }
-        else if ($scope.quiz.question.type == 'shortAnswer' && $scope.quiz.question.answerText != '') {
-            if (typeof submittedAnswers[$scope.quiz.currentQuestion] === 'undefined') {
-                var uqqa = {
-                    quizId: quizId,
-                    questionId: angular.copy($scope.quiz.question.questionId),
-                    answerId: angular.copy($scope.quiz.question.answers[0].id),
-                    answerText: angular.copy($scope.quiz.question.answerText)
-                }
-                submittedAnswers.push(uqqa);
-            }
-            else {
-                submittedAnswers[$scope.quiz.currentQuestion].answerText = angular.copy($scope.quiz.question.answerText);
-            }
-        }
+        $('#confirm-submit').modal('hide');
+        $http.post('/Training/SubmitGrade', JSON.stringify({ decisions: gradeApprovals }), config)
     }
-
-    function setScope() {
-        $scope.quiz.questions
-        $scope.quiz.question.questionId = data.questions[$scope.quiz.currentQuestion].id;
-        $scope.quiz.question.questionText = data.questions[$scope.quiz.currentQuestion].text;
-        $scope.quiz.question.type = data.questions[$scope.quiz.currentQuestion].type;
-        $scope.quiz.question.answerText = '';
-        $scope.quiz.question.selectedAnswer = 0;
-        $scope.quiz.question.answers = [];
-    }*/
 });
 
